0be1e3e598397cd5cd6694263464544cee6f86d2
Yuliia2991 <yuliiakurian@gmail.com>
2022-05-28 13:12:21 +0300

    Add SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
 
